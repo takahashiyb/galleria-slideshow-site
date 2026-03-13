790f0b93f3f40c097db23a2ea0b0632186468b39
takahashiyb <takahashiyb89@gmail.com>
2026-03-13 17:28:32 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { onBeforeMount } from 'vue'
+import { useDataStore } from './stores/data'
+
+const data = useDataStore()
+
+onBeforeMount(async () => {
+  const res = await fetch(import.meta.env.BASE_URL + '/assets/data/data.json')
+  data.json = await res.json()
+})
+</script>
+
+<template>
+  <router-view />
+</template>
+
+<style>
+body {
+  margin: 0;
+}
+</style>

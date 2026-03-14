f3c475512be9615f3409725cc9e9b12850a178ae
takahashiyb <takahashiyb89@gmail.com>
2026-03-14 19:21:30 +0800

    Set up router config

--- src/pages/DetailsPage.vue
+++ src/pages/DetailsPage.vue
@@ -1,13 +1,25 @@
 <script lang="ts" setup>
-import FooterSection from '@/components/FooterSection.vue'
 import HeaderSection from '@/components/HeaderSection.vue'
 import HeroImage from '@/components/HeroImage.vue'
+import FooterSection from '@/components/FooterSection.vue'
+
 import { useDataStore } from '@/stores/data'
+import { useDialogStore } from '@/stores/dialog'
+import DialogSection from '@/components/DialogSection.vue'
 
 const data = useDataStore()
+
+const dialog = useDialogStore()
+
 </script>
+
 <template>
+  <DialogSection v-if="data.isDataReady" />
   <HeaderSection v-if="data.isDataReady" />
   <HeroImage v-if="data.isDataReady" />
   <FooterSection v-if="data.isDataReady" />
 </template>
+
+<style lang="scss" scoped>
+@use '@/assets/styles/main.scss' as v;
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,16 @@
 import DetailsPage from '@/pages/DetailsPage.vue'
 import HomePage from '@/pages/HomePage.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { name: 'home', path: '/', component: HomePage },
-    { name: 'details', path: '/details/:name', component: DetailsPage },
+    {
+      name: 'details',
+      path: '/details/:name',
+      component: DetailsPage,
+    },
   ],
 })
 

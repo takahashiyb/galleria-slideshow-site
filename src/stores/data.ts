da4ec3f6665a8901262a98cfbf57365eb87ef814
takahashiyb <takahashiyb89@gmail.com>
2026-03-14 19:20:12 +0800

    add states that will allow the tracking of the current page and functions to switch between them.

--- src/stores/data.ts
+++ src/stores/data.ts
@@ -13,5 +13,43 @@ export const useDataStore = defineStore('data', () => {
     return json.value[lastSeen.value]!.name.replace(/ /g, '').trim()
   })
 
-  return { json, isDataReady, lastSeen, getLastSeen }
+  const getCurrentPage = computed(() => {
+    return json.value.find((i) => getLastSeen.value === i.name.replace(/ /g, '').trim()) as Painting
+  })
+
+  function findCurrentPage(param: string) {
+    return json.value.find((i) => param === i.name.replace(/ /g, '').trim())
+  }
+
+  function findCurrentIndex(param: string) {
+    return json.value.findIndex((i) => param === i.name.replace(/ /g, '').trim())
+  }
+
+  function getNext(currentIndex: number) {
+    if (json.value.length <= currentIndex + 1) {
+      return json.value[0]!.name.replace(/ /g, '').trim()
+    }
+
+    return json.value[currentIndex + 1]!.name.replace(/ /g, '').trim()
+  }
+
+  function getBefore(currentIndex: number) {
+    if (0 > currentIndex - 1) {
+      return json.value[json.value.length - 1]!.name.replace(/ /g, '').trim()
+    }
+
+    return json.value[currentIndex - 1]!.name.replace(/ /g, '').trim()
+  }
+
+  return {
+    json,
+    isDataReady,
+    lastSeen,
+    getLastSeen,
+    getCurrentPage,
+    findCurrentPage,
+    findCurrentIndex,
+    getNext,
+    getBefore,
+  }
 })
